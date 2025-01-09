d94fa9e40c16d36598345362d10ef3fa324bef5f
Hong Minhee <hong@minhee.org>
2025-01-10 01:10:22 +0900

    Re-export KV/MQ drivers for development purpose

--- src/mod.ts
+++ src/mod.ts
@@ -14,6 +14,12 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 export {
+  InProcessMessageQueue,
+  type InProcessMessageQueueOptions,
+  MemoryKvStore,
+  ParallelMessageQueue,
+} from "@fedify/fedify/federation";
+export {
   Application,
   type Bot,
   type BotKvPrefixes,
